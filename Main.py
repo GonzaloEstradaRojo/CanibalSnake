39f4e74dd423c718f61bd564e3e2e5db5a14b02d
Gonzalo Estrada Rojo <48635918+GonzaloEstradaRojo@users.noreply.github.com>
2023-02-12 01:52:03 +0100

    Snake Draw and Move implemented

--- Main.py
+++ Main.py
@@ -1,36 +1,50 @@
 import pygame, random, math
-
+import Snake as s
 
 width, height = 800, 800
-FPS = 30
-run, gameOver = True, False
-cols,rows = 30,30
-rectW, rectH = width/cols, height/rows
-
-screen = pygame.display.set_mode((width, height))
-board = [[0 for _ in range(cols)] for _ in range(rows)]
+cols,rows = 25,25
+FPS = 10
+running, gameOver = True, False
+rectW = width/cols
 
 pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((width, height))
+# screen.fill((100,100,100))
 
+board = [[0 for _ in range(cols)] for _ in range(rows)]
 
 def ResetBoard():
     global board
     board = [[0 for _ in range(cols)] for _ in range(rows)]
     
 def DrawBoard():
+    screen.fill((255,255,255))
     for i in range(len(board)):
-        for j in range(len(board)[i]):
-            pygame.draw.rect
+        for j in range(len(board[i])):
+
+            rect = pygame.Rect(i*rectW, j*rectW, rectW, rectW)
+            pygame.draw.rect(screen, (100,100,100), rect,1)
 def GameLoop():
     global running, gameOver
     
-    events = pygame.event.get()
-    for event in events:
-        if event.type == pygame.QUIT:
-            running = False
-        if pygame.key.get_pressed()[pygame.K_r]:
-            GameLoop()
-     
+    s1 = s.Snake(screen, (0,0),rectW)
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                s1.ChangeDirection(event)
+            if pygame.key.get_pressed()[pygame.K_r]:
+                ResetBoard()
+                GameLoop()
+        DrawBoard()
+        s1.update()
+        
+        pygame.display.update()
+        clock.tick(FPS)
+    pygame.quit()
 
 if __name__ == "__main__":
     GameLoop()
--- Snake.py
+++ Snake.py
@@ -1,5 +1,76 @@
 import pygame, random, math
 
+UP = 0
+RIGTH = 1
+DOWN = 2
+LEFT = 3
+
 class Snake(pygame.sprite.Sprite):
-     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
+
+   def __init__(self, surface, pos, width):
+      pygame.sprite.Sprite.__init__(self)
+      self.pos = pos
+      self.width = width
+      self.rect = pygame.Rect(0,0, self.width, self.width)
+      self.surface = surface
+      self.dir = RIGTH   
+      self.score = 0
+      self.body = []
+
+   
+   def MoveSnake(self):
+      if self.dir == RIGTH:
+         self.pos = (self.pos[0] + self.width, self.pos[1])
+      elif self.dir == DOWN:
+         self.pos = (self.pos[0], self.pos[1] + self.width)
+      elif self.dir == LEFT:
+         self.pos = (self.pos[0] - self.width, self.pos[1])
+      elif self.dir == UP:
+         self.pos = (self.pos[0], self.pos[1] - self.width)
+      self.DrawSnake()
+
+   def ChangeDirection(self, event):
+      if event.key == pygame.K_UP:
+         print("UP")
+         self.dir = UP
+      elif event.key == pygame.K_RIGHT:
+         print("RIGTH")
+         self.dir = RIGTH
+      elif event.key == pygame.K_DOWN:
+         print("DOWN")
+         self.dir = DOWN
+      elif event.key == pygame.K_LEFT:
+         print("LEFT")
+         self.dir = LEFT
+
+   def DrawSnake(self):
+      # self.surface.fill((100,100,100))
+      pygame.draw.rect(self.surface, (0,255,0), pygame.Rect(self.pos[0], self.pos[1], self.width, self.width))
+
+   def CheckBorderCollision(self):
+      w, h = self.surface.get_size()
+      if self.pos[0] + self.width > w:
+         self.pos = (0, self.pos[1])
+      
+      if self.pos[0] < 0:
+         self.pos = (w - self.width, self.pos[1])
+
+      if self.pos[1] + self.width > h:
+         self.pos = (self.pos[0], 0)
+      
+      if self.pos[1] < 0:
+         self.pos = (self.pos[0], h - self.width)
+      
+
+
+
+   def AddPiece(self):
+      pass
+
+   def EatFruit():
+      pass 
+
+   def update(self):
+      self.MoveSnake()
+      self.CheckBorderCollision()
+      self.DrawSnake()
